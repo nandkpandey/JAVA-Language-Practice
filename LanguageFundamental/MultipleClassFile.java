6fde1cf2e648dabef32ee74285d18bef293b6fe4
nandkpandey <nandkpandey89@gmail.com>
2024-01-17 17:15:39 +0000

    Multiple class File

---
+++ LanguageFundamental/MultipleClassFile.java
@@ -0,0 +1,13 @@
+ class File1{
+//Can not be public Class
+}
+
+
+
+public class MultipleClassFile {
+   //File .java name Should be the Class Which contains main method 
+public static void main(String[] args) {
+    
+}
+
+}
